a3519863fb4478cda2ef600e269f47337ec8e0ba
Wesley George <wgeorge@bionym.com>
2015-06-16 20:47:04 -0400

    The start of a beautiful new python project!

---
+++ my_awesome_script.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+def some_function():
+	print "I'm a function"
+
+if __name__ == "__main__":
+	print "Oh hello world! This is '%s' reporting for duty!" % __file__
+	print "I'm executing as main!"
