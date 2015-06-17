6b7e4b2ca8c6803a1d99b16c7e6abbabc7a52855
Wesley George <wgeorge@bionym.com>
2015-06-16 21:15:51 -0400

    PEP8 compliance

--- my_awesome_script.py
+++ my_awesome_script.py
@@ -2,8 +2,8 @@
 
 ## make this compatible for python3
 def some_function():
-	print ("I'm a function")
+	print("I'm a function")
 
 if __name__ == "__main__":
-	print ("Oh hello world! This is '%s' reporting for duty!" % __file__)
-	print ("I'm executing as main!")
+	print("Oh hello world! This is '%s' reporting for duty!" % __file__)
+	print("I'm executing as main!")
