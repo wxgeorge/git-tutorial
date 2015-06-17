e9733818ed90968b68b2fdc8c82ec5c5e7a5c25d
Fort McGregor <matthew.mcgraw@gmail.com>
2015-06-16 21:09:53 -0400

    Updates to convert to Python3

--- my_awesome_script.py
+++ my_awesome_script.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
+## make this compatible for python3
 def some_function():
-	print "I'm a function"
+	print ("I'm a function")
 
 if __name__ == "__main__":
-	print "Oh hello world! This is '%s' reporting for duty!" % __file__
-	print "I'm executing as main!"
+	print ("Oh hello world! This is '%s' reporting for duty!" % __file__)
+	print ("I'm executing as main!")
